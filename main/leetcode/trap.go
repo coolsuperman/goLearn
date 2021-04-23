43cc4b2cc19e2a05dfaf631e13f7eeb4202de062
huanghaoze <huanghaoze@changba.com>
2021-04-23 18:47:37 +0800

    maint:leetcode 42 接雨水

---
+++ main/leetcode/trap.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	fmt.Printf("%d\n", trap(height))
+}
+
+func trap(height []int) int {
+	var stack []int
+	ret := 0
+	for n, v := range height {
+		count := len(stack)
+		if count == 0 {
+			stack = append(stack, n)
+		} else if count == 1 {
+			stop := stack[count-1]
+			if height[stop] < v {
+				stack = []int{}
+			}
+			stack = append(stack, n)
+		} else {
+			for count > 1 {
+				stop := stack[count-1]
+				if height[stop] < v {
+					sindex := stack[count-2]
+					ret += (min(height[sindex], v) - height[stop]) * (n - sindex - 1)
+					stack = stack[:count-1]
+					count -= 1
+				} else {
+					stack = append(stack, n)
+					count += 1
+					break
+				}
+			}
+			if count == 1 {
+				if height[stack[count-1]] < v {
+					stack = []int{n}
+				} else {
+					stack = append(stack, n)
+				}
+			}
+		}
+	}
+	return ret
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	} else {
+		return a
+	}
+}
